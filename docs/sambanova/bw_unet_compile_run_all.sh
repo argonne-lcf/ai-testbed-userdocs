b5d6c3ce0c6a59bf0db3273e26bd6289e172241e
Bruce Ray Wilson <BruceRayWilson@anl.gov>
2022-11-23 10:18:39 -0700

    touch

--- docs/sambanova/bw_unet_compile_run_all.sh
+++ docs/sambanova/bw_unet_compile_run_all.sh
@@ -31,28 +31,18 @@ if [ ! -e out/unet_train_${BS}_${ARG2}_NP/unet_train_${BS}_${ARG2}_NP.pef ] ; th
 fi
 
 
-#run loop
+#run
+
 #single
-for BS in ${ARG3}
-do
-  export OMP_NUM_THREADS=16
-  #run single
-  #srun --cpus-per-task=16 --gres=rdu:1 python ${UNET}/unet_hook.py  run --num-workers=${NUM_WORKERS} --do-train --in-channels=3 --in-width=${ARG2} --in-height=${ARG2} --init-features 32 --batch-size=${BS} --epochs 10  --data-dir /usr/local/share/data/unet32 --log-dir log_dir_unet_train_kaggle --pef=$(pwd)/out/unet_train_${BS}_${ARG2}_single/unet_train_${BS}_${ARG2}_single.pef --use-sambaloader > run_unet_${BS}_${ARG2}_single.log 2>&1
-  #Parallel
-  for NP in 2
-  do
-    echo "RUN"
-    echo "NP=${NP}"
-    #rm -rf log_dir*
-    #export SAMBA_CCL_USE_PCIE_TRANSPORT=1
-    #export SF_RNT_NUMA_BIND=2
-    #export SF_RNT_FSM_POLL_BUSY_WAIT=1
-    #export SF_RNT_DMA_POLL_BUSY_WAIT=1
-    #sbatch --cpus-per-task=4 --gres=rdu:${NP} /opt/mpich-3.4.3/bin/mpirun -hosts=192.168.200.131,192.168.200.130  -np ${NP}  python ${UNET}/unet_hook.py  run --num-workers=${NUM_WORKERS} --do-train --in-channels=3 --in-width=${ARG2} --in-height=${ARG2} --init-features 32 --batch-size=${BS} --epochs 10  --data-dir /var/tmp/kaggle_3m  --log-dir log_dir_unet_${NP}_train_kaggle --pef=$(pwd)/out/unet_train_${BS}_${ARG2}_NP/unet_train_${BS}_${ARG2}_NP.pef --data-parallel --reduce-on-rdu --use-sambaloader > run_unet_${BS}_${ARG2}_${NP}.log 2>&1
-    #sbatch --cpus-per-task=16 --gres=rdu:1 --ntasks-per-node 1  --nodes 2 --mpi=pmi2 --CPU-per-job 16 ./unet_batch.sh ${NP} ${NUM_WORKERS}
-    #sbatch -n 2 --gres=rdu:1 --tasks-per-node 1  --nodes 2 --cpus-per-task=16 ./unet_batch.sh ${NP} ${NUM_WORKERS}
-    sbatch --gres=rdu:1 --tasks-per-node 8  --nodes 2 --nodelist sm-02,sm-01 --cpus-per-task=16 ./unet_batch.sh ${NP} ${NUM_WORKERS}
-  done
-done
+BS=${ARG3}
+export OMP_NUM_THREADS=16
+#run single
+#srun --cpus-per-task=16 --gres=rdu:1 python ${UNET}/unet_hook.py  run --num-workers=${NUM_WORKERS} --do-train --in-channels=3 --in-width=${ARG2} --in-height=${ARG2} --init-features 32 --batch-size=${BS} --epochs 10  --data-dir /usr/local/share/data/unet32 --log-dir log_dir_unet_train_kaggle --pef=$(pwd)/out/unet_train_${BS}_${ARG2}_single/unet_train_${BS}_${ARG2}_single.pef --use-sambaloader > run_unet_${BS}_${ARG2}_single.log 2>&1
+
+#Parallel
+NP=2
+echo "RUN"
+echo "NP=${NP}"
+sbatch --gres=rdu:1 --tasks-per-node 8  --nodes 2 --nodelist sm-02,sm-01 --cpus-per-task=16 ./unet_batch.sh ${NP} ${NUM_WORKERS}
 echo "Duration: " $SECONDS
 
